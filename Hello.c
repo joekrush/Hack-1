880de29324273a62d0c421058ba8ca08aafa9dd4
joekrush <42564550+joekrush@users.noreply.github.com>
2018-08-23 10:37:50 -0500

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Joe Krushenisky, Computer Science, Grant Baumhover Software Engineer\n");
+  printf("Joe Krushenisky Computer Science, Grant Baumhover Software Engineer\n");
 
   return 0;
 }
